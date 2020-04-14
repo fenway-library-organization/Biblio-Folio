850ab210f86f8414dbf92d85789a0421d45bc458
Paul Hoffman <paul@flo.org>
2020-04-14 16:40:52 -0400

    distinguish between matchpoints per se and check fields

--- lib/Biblio/Folio/Site/LoadProfile.pm
+++ lib/Biblio/Folio/Site/LoadProfile.pm
@@ -1,5 +1,20 @@
 package Biblio::Folio::Site::LoadProfile;
 
+use strict;
+use warnings;
+
+@Biblio::Folio::Site::LoadProfile::ISA = qw(Biblio::Folio::Object);
+
+use Biblio::Folio::Util qw(
+	_tok2const
+	_cql_value
+	_cql_term
+	_cql_and
+	_cql_or
+    _get_attribute_from_dotted
+);
+use Text::Balanced qw(extract_delimited);
+
 sub init {
     my ($self) = @_;
     $self->SUPER::init;
@@ -10,7 +25,9 @@ sub init {
         my %field = (
             'field' => $f,
             'qualifier' => 'any',
-            'required' => 0,
+            'is_matchpoint' => 0,
+            'is_check' => 0,
+            'is_required' => 0,
             'order' => 1<<15,
             'exact' => 1,
         );
@@ -19,7 +36,19 @@ sub init {
             if ($n++) {
                 s/^\s*,\s+// or die "invalid field parameters: $f = $fields->{$f}";
             }
-            if (s/^copy from (\w+)//) {
+            if (s/^matchpoint//) {
+                $field{'is_matchpoint'} = 1;
+            }
+            elsif (s/^check//) {
+                $field{'is_check'} = 1;
+            }
+            elsif (s/^required//) {
+                $field{'is_required'} = 1;
+            }
+            elsif (s/^optional//) {
+                $field{'is_required'} = 0;
+            }
+            elsif (s/^copy from (\w+)//) {
                 $field{'copy_from'} = $1;
             }
             elsif (s/^(?:in|not )exact//) {
@@ -27,26 +56,61 @@ sub init {
             }
             elsif (s/^default://) {
                 my $dv = extract_value();
-                die "invalid default: $_" if !defined $dv;
+                die "invalid default in field spec: $f = $fields->{$f}" if !defined $dv;
                 $field{'default'} = $dv;
             }
             elsif (s/^([^:,\s]+)//) {
                 my ($pk, $pv) = ($1, 1);
                 if (s/^:\s*//) {
                     $pv = extract_value();
-                    die "invalid value for $pk: $_" if !defined $pv;
+                    die "invalid value for $pk in field spec: $f = $fields->{$f}" if !defined $pv;
                 }
                 # if ($pk =~ /^(lowest|highest)$/) {
                 #     ($pk, $pv) = ('qualifier', _matchpoint_qualifier($pk));
                 # }
                 $field{$pk} = $pv;
             }
+            elsif (/\S/) {
+                die "unparseable field spec: $f = $fields->{$f}";
+            }
         }
-        die "contradictory mode for $type: field $f both required and optional"
-            if $field{'required'} && $field{'optional'};
+        die "contradictory mode for $type: field $f both required and optional: $fields->{$f}"
+            if $field{'is_required'} && $field{'is_optional'};
         $fields->{$f} = \%field;
     }
     return $self;
+}
+
+sub fields {
+    my ($self, $obj) = @_;
+    my $fields = $self->{'fields'};
+    if (!defined $obj) {
+        return values %$fields if wantarray;
+        return $fields;
+    }
+    my (@fields, %field);
+    while (my ($f, $field) = each %$fields) {
+        my $val = _get_attribute_from_dotted($obj, $field->{'from'} || $f);
+        $val = $field->{'default'} if !defined $val;
+        my @values = map { defined $_ ? $_ : $field->{'default'} } ref($val) eq 'ARRAY' ? @$val : ($val);
+        foreach my $v (@values) {
+            next if !defined $v || !length $v;
+            my $cqlv = _cql_value($v);
+            my $field = {
+                %$field,
+                'value' => $v,
+                'cqlvalue' => $cqlv,
+            };
+            if (wantarray) {
+                push @fields, $field;
+            }
+            else {
+                $field{$f}{$cqlv} = $field;
+            }
+        }
+    }
+    return @fields if wantarray;
+    return \%field;
 }
 
 sub extract_value {
@@ -57,7 +121,7 @@ sub extract_value {
         $v = $2;
     }
     elsif (s/^(true|false|null)//) {
-        $v = $tok2const{$1};
+        $v = _tok2const($1);
     }
     elsif (s/^([0-9]+(?:\.[0-9]+)?)//) {
         $v = $1;
